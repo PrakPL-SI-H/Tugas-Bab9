f754b736278b52e28428a5a86883362287a426a3
istighfarinba <bafarcorp@gmail.com>
2016-05-25 19:34:48 +0700

    make FrameProgram.java dan extends default class java(Frame)

---
+++ src/FrameProgram.java
@@ -0,0 +1,9 @@
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.Color;
+import javax.swing.JRadioButton;
+
+public class FrameProgram extends Frame { //pemanggilan data di parent default dari java class Frame
+
+}
