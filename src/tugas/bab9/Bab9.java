0e3ba8e662f01e8458be477a3f0ca2aa0b07aa21
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-05-25 23:34:13 +0700

    edit main class bab9

---
+++ src/tugas/bab9/Bab9.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bab9;
+import java.awt.Color;
+/**
+ *
+ * @author Prasetyo
+ */
+public class Bab9 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GUI window = new GUI();
+        window.setTitle("Nilai");
+        window.setSize(370, 500);
+        window.show();
+    }    
+}
