6a2f858256023f566d87787b930fafbe7c72e0ef
Muh. Asdar <muh.asdar17@gmail.com>
2016-05-25 21:54:44 +0700

    memperbaiki beberapa source code dan menambah tampilan pada warna

--- src/tugas/bab9/Main.java
+++ src/tugas/bab9/Main.java
@@ -1,15 +1,10 @@
 package tugas.bab9;
-
-import java.awt.Color;
-
 public class Main {
     public static void main(String[] args) {
-        TFrame window = new TFrame();
-        window.setTitle("Hitung Nilai Ahir");
+     TFrame window = new TFrame();
+        window.setTitle("Perhitungan Rata-rata IP");
+        window.setBackground(java.awt.Color.GREEN);
         window.setSize(440, 600);
-        window.setBackground(Color.LIGHT_GRAY);
         window.show();
-
-    }
-    
+    }    
 }
--- src/tugas/bab9/TFrame.java
+++ src/tugas/bab9/TFrame.java
@@ -1,18 +1,15 @@
 package tugas.bab9;
-
 import java.awt.*;
 import java.awt.event.*;
 
 public class TFrame extends Frame {
-
-    Label lbjudul, lbtugas, lbuas, lbuts, lbkuis, lbhasil;
-    TextField tftugas, tfkuis, tfuts, tfuas, tfhasil;
-    TextArea tatampilsemua;
-    Button btnhasil, btntampilkansemua;
-    CheckboxGroup cbg;
-    Checkbox cbasd, cbpemlan, cbprobstat, cbmatkomlan;
-    public boolean asd, pemlan, matkomlan, probstat;
-    public double nilaiasd, nilaipemlan, nilaimatkomlan, nilaiprobstat;
+    Label lbJudul, lbTugas, lbKuis, lbUTS, lbUAS,lbNilaiAk;
+    TextField txtTugas, txtKuis, txtUTS, txtUAS,txtNilaiAk;
+    Button btnHasil, btnLihat;
+    Checkbox cbASD, cbPemlan, cbMatkomlan, cbProbstat;
+    CheckboxGroup cbg1;
+    TextArea txtarea1;
+    String Study, ASD = "", PEMLAN = "", MATKOMLAN = "", PROBSTAT = "";
 
     public TFrame() {
         addWindowListener(new WindowAdapter() {
@@ -21,140 +18,141 @@ public class TFrame extends Frame {
                 System.exit(0);
             }
         });
-
         setLayout(null);
-        lbjudul = new Label("~~ Hitung Nilai Akhir ~~");
-        lbjudul.setFont(new Font("Arial", 1, 14));
-        add(lbjudul);
-        lbjudul.setBounds(130, 40, 200, 20);
+        lbJudul = new Label("Hitung Nilai Akhir");
+        lbJudul.setFont(new Font("Arial", 1, 20));
+        add(lbJudul);
+        lbJudul.setForeground(Color.DARK_GRAY);
+        lbJudul.setBounds(135, 35, 350, 20);
+        lbTugas = new Label("Tugas");
+        add(lbTugas);
+        lbTugas.setForeground(Color.white);
+        lbTugas.setBounds(100, 90, 120, 20);
+        lbKuis = new Label("Kuis");
+        add(lbKuis);
+        lbKuis.setForeground(Color.white);
+        lbKuis.setBounds(100, 120, 120, 20);
+        lbUTS = new Label("UTS");
+        add(lbUTS);
+        lbUTS.setForeground(Color.white);
+        lbUTS.setBounds(100, 150, 120, 20);
+        lbUAS = new Label("UAS");
+        add(lbUAS);
+        lbUAS.setForeground(Color.white);
+        lbUAS.setBounds(100, 180, 120, 20);
+        lbNilaiAk = new Label("Nilai Akhir");
+        add(lbNilaiAk);
+        lbNilaiAk.setForeground(Color.white);
+        lbNilaiAk.setBounds(100, 210, 120, 20);
 
-        lbtugas = new Label("Tugas");
-        lbtugas.setBounds(125, 100, 70, 20);
-        add(lbtugas);
+        txtTugas = new TextField("");
+        add(txtTugas);
+        txtTugas.setBounds(240, 90, 120, 20);
+        txtKuis = new TextField("");
+        add(txtKuis);
+        txtKuis.setBounds(240, 120, 120, 20);
+        txtUTS = new TextField("");
+        add(txtUTS);
+        txtUTS.setBounds(240, 150, 120, 20);
+        txtUAS = new TextField("");
+        add(txtUAS);
+        txtUAS.setBounds(240, 180, 120, 20);
+        txtNilaiAk = new TextField("");
+        add(txtNilaiAk);
+        txtNilaiAk.setBounds(240, 210, 120, 20);
 
-        lbkuis = new Label("Kuis");
-        lbkuis.setBounds(125, 130, 70, 20);
-        add(lbkuis);
+        cbg1 = new CheckboxGroup();
+        cbASD = new Checkbox(("ASD"), cbg1, false);
+        add(cbASD);
+        cbASD.setForeground(Color.WHITE);
+        cbASD.setBounds(40, 60, 80, 20);
+        cbPemlan = new Checkbox("Pemlan", cbg1, false);
+        add(cbPemlan);
+        cbPemlan.setForeground(Color.WHITE);
+        cbPemlan.setBounds(120, 60, 80, 20);
+        cbMatkomlan = new Checkbox(("Matkomlan"), cbg1, 
+        false);
+        add(cbMatkomlan);
+        cbMatkomlan.setForeground(Color.WHITE);
+        cbMatkomlan.setBounds(220, 60, 80, 20);
+        cbProbstat = new Checkbox("Probstat", cbg1, false);
+        add(cbProbstat);
+        cbProbstat.setForeground(Color.WHITE);
+        cbProbstat.setBounds(320, 60, 80, 20);
+        cbASD.addItemListener(new mainmAT());
+        cbPemlan.addItemListener(new mainmAT());
+        cbMatkomlan.addItemListener(new mainmAT());
+        cbProbstat.addItemListener(new mainmAT());
 
-        lbuts = new Label("UTS");
-        lbuts.setBounds(125, 160, 70, 20);
-        add(lbuts);
+        btnHasil = new Button("Hasil");
+        add(btnHasil);
+        btnHasil.setBounds(240, 240, 120, 20);
+        btnHasil.addActionListener(new mainAction());
 
-        lbuas = new Label("UAS");
-        lbuas.setBounds(125, 190, 70, 20);
-        add(lbuas);
+        txtarea1 = new TextArea();
+        txtarea1.setFont(new Font("Arial", 1, 14));
+        add(txtarea1);
+        txtarea1.setBounds(60, 275, 340, 175);
 
-        lbhasil = new Label("Hasil");
-        lbhasil.setBounds(125, 220, 70, 20);
-        add(lbhasil);
-
-        tftugas = new TextField("0");
-        add(tftugas);
-        tftugas.setBounds(270, 100, 60, 20);
-
-        tfkuis = new TextField("0");
-        add(tfkuis);
-        tfkuis.setBounds(270, 130, 60, 20);
-
-        tfuts = new TextField("0");
-        add(tfuts);
-        tfuts.setBounds(270, 160, 60, 20);
-
-        tfuas = new TextField("0");
-        add(tfuas);
-        tfuas.setBounds(270, 190, 60, 20);
-
-        tfhasil = new TextField("0");
-        add(tfhasil);
-        tfhasil.setBounds(270, 220, 60, 20);
-
-        btnhasil = new Button("Hitung");
-        add(btnhasil);
-        btnhasil.setBounds(250, 250, 100, 20);
-
-        tatampilsemua = new TextArea("");
-        add(tatampilsemua);
-        tatampilsemua.setBounds(110, 300, 235, 200);
-
-        btntampilkansemua = new Button("Tampilkan Nilai Semua Matkul");
-        add(btntampilkansemua);
-        btntampilkansemua.setBounds(110, 520, 235, 25);
-
-        cbg = new CheckboxGroup();
-        cbasd = new Checkbox("ASD", cbg, false);
-        this.add(cbasd).setBounds(35, 70, 90, 20);
-        cbpemlan = new Checkbox("Pemlan", cbg, false);
-        this.add(cbpemlan).setBounds(125, 70, 90, 20);
-        cbmatkomlan = new Checkbox("Matkomlan", cbg, false);
-        this.add(cbmatkomlan).setBounds(220, 70, 90, 20);
-        cbprobstat = new Checkbox("Probstat", cbg, false);
-        this.add(cbprobstat).setBounds(330, 70, 90, 20);
-
-        cbasd.addItemListener(new mainActionChekBox());
-        cbpemlan.addItemListener(new mainActionChekBox());
-        cbmatkomlan.addItemListener(new mainActionChekBox());
-        cbprobstat.addItemListener(new mainActionChekBox());
-        btnhasil.addActionListener(new mainAction());
-        btntampilkansemua.addActionListener(new mainActionTampilsemua());
+        btnLihat = new Button("Tampilkan semua nilai matkul");
+        add(btnLihat);
+        btnLihat.setBounds(60, 475, 340, 30);
+        btnLihat.addActionListener(new mainAction2());
     }
-
-    class mainAction extends mainActionChekBox implements ActionListener {
-
-        @Override
+    class mainAction implements ActionListener {
         public void actionPerformed(ActionEvent event) {
-            double tugas = Double.parseDouble(tftugas.getText().trim());
-            double kuis = Double.parseDouble(tfkuis.getText().trim());
-            double uts = Double.parseDouble(tfuts.getText().trim());
-            double uas = Double.parseDouble(tfuas.getText().trim());
-            String hasil = "0";
-            if (asd == true) {
-                hasil = String.valueOf((tugas + kuis + uts + uas) / 4);
-                nilaiasd = Double.parseDouble(hasil);
-            } else if (pemlan == true) {
-                hasil = String.valueOf((tugas + kuis + uts + uas) / 4);
-                nilaipemlan = Double.parseDouble(hasil);
-            } else if (matkomlan == true) {
-                hasil = String.valueOf((tugas + kuis + uts + uas) / 4);
-                nilaimatkomlan = Double.parseDouble(hasil);
-            } else if (probstat == true) {
-                hasil = String.valueOf((tugas + kuis + uts + uas) / 4);
-                nilaiprobstat = Double.parseDouble(hasil);
+            double Tugas = Double.parseDouble(txtTugas.getText().trim());
+            double Kuis =  Double.parseDouble(txtKuis.getText().trim());
+            double UTS = Double.parseDouble(txtUTS.getText().trim());
+            double UAS = Double.parseDouble(txtUAS.getText().trim());
+            String NilaiAk = String.valueOf((Tugas + Kuis + UTS + UAS) / 4);
+            txtNilaiAk.setText(NilaiAk);
+
+            if (Study == "ASD") {
+                ASD = txtNilaiAk.getText().toString();
+            } else if (Study == "Pemrograman Lanjut") {
+                PEMLAN = txtNilaiAk.getText().toString();
+            } else if (Study == "Matematika Komputasi Lanjutan") {
+                MATKOMLAN = txtNilaiAk.getText().toString();
+            } else if (Study == "Probabilitas dan Statistika") {
+                PROBSTAT = txtNilaiAk.getText().toString();
             }
-            tfhasil.setText(hasil);
         }
     }
-
-    class mainActionTampilsemua extends mainAction implements ActionListener {
-
-        @Override
-        public void actionPerformed(ActionEvent event) {
-            String temp = ("\n\tHasil Nilai Semua Matkul"
-                    + "\n\n\tASD\t\t= " + nilaiasd
-                    + "\n\n\tPemlan\t\t= " + nilaipemlan
-                    + "\n\n\tMatkomlan\t= " + nilaimatkomlan
-                    + "\n\n\tProbstat\t\t= " + nilaiprobstat);
-            tatampilsemua.setText(temp);
-        }
-    }
-
-    class mainActionChekBox implements ItemListener {
-
-        @Override
+    class mainmAT implements ItemListener {
         public void itemStateChanged(ItemEvent e) {
-            Object source = e.getItemSelectable();
-            asd = false;
-            pemlan = false;
-            matkomlan = false;
-            probstat = false;
-            if (source == cbasd) {
-                asd = true;
-            } else if (source == cbpemlan) {
-                pemlan = true;
-            } else if (source == cbmatkomlan) {
-                matkomlan = true;
+            txtTugas.toString();
+            txtTugas.setText("");
+            txtKuis.toString();
+            txtKuis.setText("");
+            txtUTS.toString();
+            txtUTS.setText("");
+            txtUAS.toString();
+            txtUAS.setText("");
+            txtNilaiAk.toString();
+            txtNilaiAk.setText("");
+            Object cek = e.getItemSelectable();
+
+            if (cek == cbASD) {
+                Study = "ASD";
+            } else if (cek == cbPemlan) {
+                Study = "Pemrograman Lanjut";
+            } else if (cek == cbMatkomlan) {
+                Study = "Matematika Komputasi Lanjutan";
             } else {
-                probstat = true;
+                Study = "Probabilitas dan Statistika";
             }
+        }
+    }
+    class mainAction2 implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            String tamp = "\n\tHasil Nilai Semua Matkul" 
+                    +"\n\nASD\t\t\t\t\t: " + ASD
+                    + "\nPemrograman Lanjut\t\t: " + PEMLAN
+                    + "\nMatematika Komputasi Lanjutan\t: " + MATKOMLAN
+                    + "\nProbabilitas dan Statistika\t\t: " + PROBSTAT;
+            txtarea1.toString();
+            txtarea1.setText(tamp);
         }
     }
 }
