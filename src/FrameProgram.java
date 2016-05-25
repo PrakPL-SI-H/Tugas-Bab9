868998782e5949aa9fc8b9ca33147df011a59540
istighfarinba <bafarcorp@gmail.com>
2016-05-25 19:37:04 +0700

    deklarasi variabel untuk textarea,button,label dan checkbox nya

--- src/FrameProgram.java
+++ src/FrameProgram.java
@@ -6,4 +6,14 @@ import javax.swing.JRadioButton;
 
 public class FrameProgram extends Frame { //pemanggilan data di parent default dari java class Frame
 
+    
+    Label lbKuis, lbUTS, lbJudul, lbTugas, lbUAS, lbHasil;
+    TextField txtUTS, txtTugas, txtKuis, txtUAS, txtHasil;
+    CheckboxGroup centang;
+    TextArea txtArea;
+    Checkbox cbASD, cbProbstat, cbProglan, cbMatkomlan;
+    
+    Button btnTampil, btnHasil;
+
+
 }
