9679346d00f761d63f9665472342178833a34934
Tegar Yoga Prabawakusuma <sirtegaryoga@gmail.com>
2016-05-25 19:51:25 +0700

    pembuatan class NILAI_UJIAN dengan atribut

---
+++ src/tugas/bab9/NILAI_UJIAN.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Laporan;
+
+/**
+ *
+ * @author TEGAR
+ */
+
+ import java.awt.*;
+ import java.awt.event.*;
+ import javax.swing.JFrame;
+ 
+ public class NILAI_UJIAN extends JFrame {
+ 
+     Label lbJudul, lbTugas, lbKuis, lbUTS, lbUAS, lbHasil;
+     TextField txtTugas, txtKuis, txtUTS, txtUAS, txtHasil;
+     TextArea hasiltotal;
+     CheckboxGroup cbg;
+     Checkbox cbASD, cbPemlan, cbMatkomlan, cbProbstat;
+     Button btnHasil, btnHasilTotal;
+     public String hasilASD = "0.0", hasilPemlan = "0.0", hasilMatkomlan = "0.0", hasilProbstat = "0.0";
+ 
+ }
