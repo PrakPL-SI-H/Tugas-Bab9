e9c21a86f289976f0ea9b6ad7590ed2dd1c8423e
Aldo Maulana Hidayat <aldzmau@gmail.com>
2016-05-25 17:03:32 +0700

    add main

---
+++ src/tugas/bab9/GUI.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prakproglan.bab9;
+
+/**
+ *
+ * @author Aldo
+ */
+public class GUI {
+    public static void main(String[] args) {
+        TFrame window = new TFrame(); 
+        window.setTitle("Hitung Nilai Akhir dengan GUI java.awt");         
+        window.setSize(400, 500);         
+        window.show(); 
+}
+}
