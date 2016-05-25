9ecb1d48e52b203b5c20abc3ff5b1101bad9cdda
istighfarinba <bafarcorp@gmail.com>
2016-05-25 19:38:54 +0700

    deklarasi array untuk tampilan akhir(agar Sederhana)

--- src/FrameProgram.java
+++ src/FrameProgram.java
@@ -6,7 +6,7 @@ import javax.swing.JRadioButton;
 
 public class FrameProgram extends Frame { //pemanggilan data di parent default dari java class Frame
 
-    
+    String[][] output = new String[4][2];//untuk outputan tampil pada textArea
     Label lbKuis, lbUTS, lbJudul, lbTugas, lbUAS, lbHasil;
     TextField txtUTS, txtTugas, txtKuis, txtUAS, txtHasil;
     CheckboxGroup centang;
