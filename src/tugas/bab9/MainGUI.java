139445a2a2c9ecefb681c5390e2e19f9861e585c
dyahas <arifiani.dyah@gmail.com>
2016-05-25 20:50:57 +0700

    main class

---
+++ src/tugas/bab9/MainGUI.java
@@ -0,0 +1,13 @@
+package maingui;
+
+public class MainGUI {
+
+    public static void main(String[] args) {
+        FrameGUI window = new FrameGUI();
+        window.setTitle("Hitung Nilai Akhir dengan GUI java.awt");
+        window.setSize(350, 550);
+        window.show();
+
+    }
+
+}
