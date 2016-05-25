d83721e7308f2c9fc5ec794d69ae20bf1b10b091
Nofry Arman <Nofry Arman>
2016-05-24 19:45:49 -0700

    ganti nama class dan tambah variabel

---
+++ src/PraktikumBab9/Frame1.java
@@ -0,0 +1,15 @@
+package PraktikumBab9;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.ButtonGroup;
+import javax.swing.JRadioButton;
+
+public class Frame1 extends Frame{
+    ButtonGroup cbg;
+    JRadioButton cbPemlan, cbASD, cbMatkom, cbProbstat;
+    Label lbJudul, lbTugas, lbKuis, lbUTS, lbUAS, lbHasil;
+    TextField txtTugas, txtKuis, txtUTS, txtUAS, txtHasil;
+    String asd, pemlan, matkom, probstat;
+    Button btnHasil, btnTampilkan;
+    TextArea tampil;
+}
