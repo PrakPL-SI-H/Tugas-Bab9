37c880fb78602342c928c61e102ecacfe405eb84
anderson manurung <andersonmanurung10@gmail.com>
2016-05-25 16:53:34 +0700

    terakhir kita membuat main class nya

---
+++ src/tugas/bab9/MainFrame.java
@@ -0,0 +1,9 @@
+package GUI;
+import javax.swing.JFrame;
+public class MainFrame {
+    private static JFrame frame;
+    public static void main(String[] args) {
+        frame = new Frame();
+        frame.setBounds(200, 200, 450, 600);
+    }
+}
