5582e99d67a3990832a1a4e0779fce0e413fba5c
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-05-25 23:28:41 +0700

    edit frame class

---
+++ src/tugas/bab9/GUI.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bab9;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.JFrame.*;
+/**
+ *
+ * @author Prasetyo
+ */
+public class GUI extends Frame {
+    Checkbox cbPROBSTAT, cbASD, cbMATKOMLAN, cbPEMLAN;
+    Label lbJudul, lbTugas, lbKuis, lbUTS, lbUAS, lbHasil;
+    TextField txtTugas, txtKuis, txtUTS, txtUAS, txtHasil;
+    CheckboxGroup cbg;
+    TextArea hasiltotal;
+    Button btnHasil,btnHasil2;
+    public String hasilASD = "0.0", hasilPemlan = "0.0", hasilMatkomlan = "0.0", hasilProbstat = "0.0";
+
+}
