68046a8fd698ece9ec5a0860012521ab98d8c4f5
Aldo Maulana Hidayat <aldzmau@gmail.com>
2016-05-25 17:25:08 +0700

    add label, textfield, textarea

--- src/tugas/bab9/TFrame.java
+++ src/tugas/bab9/TFrame.java
@@ -30,5 +30,50 @@ public class TFrame extends Frame{
         });
         
         setLayout(null);
+        
+        lbJudul = new Label("Hitung Nilai Akhir");
+        lbJudul.setFont(new Font("Arial", 1, 14)); 
+        add(lbJudul);
+        lbJudul.setBounds(140, 50, 150, 20);
+        
+        lbTugas = new Label("Tugas");
+        add(lbTugas);
+        lbTugas.setBounds(100, 115, 100, 20);
+        txtTugas = new TextField("0");
+        add(txtTugas);
+        txtTugas.setBounds(200, 115, 100, 20);
+        
+        lbKuis = new Label("Kuis");
+        add(lbKuis);
+        lbKuis.setBounds(100, 145, 100, 20);
+        txtKuis = new TextField("0");
+        add(txtKuis);
+        txtKuis.setBounds(200, 145, 100, 20);
+        
+        lbUTS = new Label("UTS");
+        add(lbUTS);
+        lbUTS.setBounds(100, 175, 100, 20);
+        txtUTS = new TextField("0");
+        add(txtUTS);
+        txtUTS.setBounds(200, 175, 100, 20);
+        
+        lbUAS = new Label("UAS");
+        add(lbUAS);
+        lbUAS.setBounds(100, 205, 100, 20);
+        txtUAS = new TextField("0");
+        add(txtUAS);
+        txtUAS.setBounds(200, 205, 100, 20);
+        
+        lbHasil = new Label("Hasil");
+        add(lbHasil);
+        lbHasil.setBounds(100, 235, 100, 20);
+        txtHasil = new TextField("0");
+        add(txtHasil);
+        txtHasil.setBounds(200, 235, 100, 20);
+        
+        txtSemua = new TextArea();
+        add(txtSemua);
+        txtSemua.setBounds(50, 300, 300, 150);
+        
     }
 }
