bc1676fc5e396d524d87fbb97f539a0eb2e28ca0
Lasmatriani <lasmatriani@gmail.com>
2016-05-25 22:12:56 +0700

    Class Main

---
+++ src/tugas/bab9/FrameFixMain.java
@@ -0,0 +1,13 @@
+package Bab9;
+
+import java.awt.*;
+
+public class FrameFixMain extends javax.swing.JFrame {
+
+    public static void main(String args[]) {
+        FrameFix window = new FrameFix();
+        window.setTitle("Hitung Nilai Akhir dengan GUI java.awt");
+        window.setSize(365, 530);
+        window.show();
+    }
+}
