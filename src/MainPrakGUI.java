76b3e4b725f3052632cee3ac4ae0be36f44f09ca
salsasyafta <salsasyafta@gmail.com>
2016-05-25 23:38:40 +0700

    membuat main classnya

---
+++ src/MainPrakGUI.java
@@ -0,0 +1,9 @@
+package GUI;
+public class MainPrakGUI {
+    public static void main(String[] args) {
+        PrakGUI gui = new PrakGUI();
+        gui.setTitle("Hitung Nilai Akhir dengan GUI.awt");
+        gui.setSize(430, 600);
+        gui.show();
+    }
+}
