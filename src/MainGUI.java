a269b0d047e984c7d271671b8f01ae7d2d0bd3a5
nandafiestafarada <nandafiestafarada@gmail.com>
2016-05-25 22:31:37 +0700

    selesai

---
+++ src/MainGUI.java
@@ -0,0 +1,11 @@
+package GUI;
+
+public class MainGUI {
+
+    public static void main(String[] args) {
+        MataKuliah window = new MataKuliah();
+        window.setTitle("Hitung Nilai Akhir dengan GUI");
+        window.setSize(350, 550);
+        window.show();
+    }
+}
