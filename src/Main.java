60a6900c5f88ac0806de9be867cd510c7b3ca684
istighfarinba <bafarcorp@gmail.com>
2016-05-25 19:56:06 +0700

    setting background layer!

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
         tampilanLayer.setTitle("PROGRAM PENGHITUNGAN NILAI AKHIR");
         tampilanLayer.setSize(410, 495);
         tampilanLayer.show();
-        
+        tampilanLayer.setBackground(new Color(51,255,0));
 
     }
 
