98ece37a50b01bf68c6274e66a163ebd7bc8353c
istighfarinba <bafarcorp@gmail.com>
2016-05-25 19:55:28 +0700

    peng-set an nama layer(window) dan ukuran layer serta memanggil method .show agar program dapat keluar

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,11 @@ public class Main {
     public static void main(String[] args) {
 
         FrameProgram tampilanLayer = new FrameProgram();
-       
+        
+        tampilanLayer.setTitle("PROGRAM PENGHITUNGAN NILAI AKHIR");
+        tampilanLayer.setSize(410, 495);
+        tampilanLayer.show();
+        
 
     }
 
