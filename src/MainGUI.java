36cd6c7f9fcbac863914f93b4b19af2961fc7c8b
MochammadAK <pagarberkarat@gmail.com>
2016-05-26 18:25:04 +0700

    2

---
+++ src/MainGUI.java
@@ -0,0 +1,9 @@
+package bab9;
+public class MainGUI {
+    public static void main(String[] args) {
+        PraktikumGUI gui = new PraktikumGUI();
+        gui.setTitle("Hitung Nilai Akhir Dengan GUI.awt");
+        gui.setSize(430, 600);
+        gui.show();
+}
+}
