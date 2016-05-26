6bbfa4a6e6b5c1fcc9f8973222d57025444f3b0a
fitriresdiyani <fitri.dyani96@gmail.com>
2016-05-27 05:35:17 +0700

    f

---
+++ src/tugas/bab9/MainFrame.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bab9;
+
+/**
+ *
+ * @author ACER
+ */
+public class MainFrame {
+
+    public static void main(String[] args) {
+        HitungNilai window = new HitungNilai();
+        window.setTitle("Hitung Nilai Akhir dengan GUI java.awt");
+        window.setSize(400, 550);
+        window.setVisible(true);
+        window.show();
+    }
+}
