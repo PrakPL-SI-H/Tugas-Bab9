296d6ff9dce58cfe36f319380286b1f151095d89
FarisGenis <faaris513@gmail.com>
2016-05-26 21:05:25 +0700

    4

---
+++ src/Bab9/Box.java
@@ -0,0 +1,18 @@
+package Bab9;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.ButtonGroup;
+import javax.swing.JRadioButton;
+
+public class Box {
+
+    Label LbJudul, LbTugas, LbKuis, LbUts, LbUas, LbHasil;
+    JRadioButton RbAsd, RbPl, RbMatkomlan, RbProbstat;
+    TextField TxtTugas, TxtKuis, TxtUts, TxtUas, TxtHasil;
+    String Asd, Pemlan, Matkomlan, Probstat;
+    Button BtnHasil, BtnShow;
+    ButtonGroup Btn;
+    TextArea TBox;
+    int Tugas, Kuis, UTS, UAS;
+}
