2600d57ee0473f7e5a58840deea90cb262cd8143
rodhirf <rodhirobb@gmail.com>
2016-05-26 08:01:51 +0700

    membuat main class

--- src/tugas/bab9/TugasBab5.java
+++ src/tugas/bab9/TugasBab5.java
@@ -1,21 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tugas.bab5;
-
-/**
- *
- * @author irwin deriyan
- */
-public class TugasBab5 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+public class mainGUI {
+public static void main(String[] args) {
+guiKuliah matkul = new guiKuliah();
+matkul.setTitle("Hitung Nilai Akhir Kuliah memakai GUI");
+matkul.setSize(500, 700);    
+}
 }
