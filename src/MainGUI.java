0bf64cd302edd44a30349be569e71cb31f0743de
ambarsari <ambardwi010@yahoo.com>
2016-06-02 17:54:44 +0700

    2

---
+++ src/MainGUI.java
@@ -0,0 +1,8 @@
+public class MainGUI {
+    public static void main(String[] args) {
+        TugasPraktikumGUI gui = new TugasPraktikumGUI();
+        gui.setTitle("Hitung Nilai Akhir Dengan GUI.awt");
+        gui.setSize(430, 600);
+        gui.show();
+}
+}
